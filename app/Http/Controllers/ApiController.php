4b5e4a794eb52f66ca8a56c1eafeea7bfe4eed69
Rafael Esaa <esaarafael@hotmail.com>
2019-06-18 12:09:11 -0400

    section login, logout, redirection url

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,104 @@
+<?php
+ 
+namespace Barbacoa\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Barbacoa\Http\Requests\RegisterAuthRequest;
+use Barbacoa\User;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Providers\JWTAuthServiceProvider;
+ 
+class ApiController extends Controller
+{
+    public $loginAfterSignUp = true;
+ 
+    public function register(RegisterAuthRequest $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->save();
+ 
+        if ($this->loginAfterSignUp) {
+            return $this->login($request);
+        }
+ 
+        return response()->json([
+            'success' => true,
+            'data' => $user
+        ], 200);
+    }
+ 
+    public function login(Request $request)
+    {
+        $input = $request->only('email', 'password');
+        $jwt_token = null;
+
+        try {
+            if (! $jwt_token = JWTAuth::attempt($input)) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Invalid Email or Password',
+                ], 401);
+            }
+            return response()->json([
+                'success' => true,
+                'token' => $jwt_token,
+            ]);
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'could_not_create_token'], 500);
+        }
+
+        return response()->json(compact('token'));
+    }
+ 
+    public function logout(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+ 
+        try {
+            JWTAuth::invalidate($request->token);
+ 
+            return response()->json([
+                'success' => true,
+                'message' => 'User logged out successfully'
+            ]);
+        } catch (JWTException $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, the user cannot be logged out'
+            ], 500);
+        }
+    }
+ 
+    public function getAuthUser(Request $request)
+    {
+        $this->validate($request, [
+            'token' => 'required'
+        ]);
+ 
+        $user = JWTAuth::authenticate($request->token);
+        return response()->json(['user' => $user]);
+    }
+
+    public function getAuthenticatedUser(){
+            try {
+                if (! $user = JWTAuth::parseToken()->authenticate()) {
+                        return response()->json(['user_not_found'], 404);
+                }
+            } catch (Tymon\JWTAuth\Exceptions\TokenExpiredException $e) {
+                return response()->json(['token_expired'], $e->getStatusCode());
+            } catch (Tymon\JWTAuth\Exceptions\TokenInvalidException $e) {
+                return response()->json(['token_invalid'], $e->getStatusCode());
+            } catch (Tymon\JWTAuth\Exceptions\JWTException $e) {
+                return response()->json(['token_absent'], $e->getStatusCode());
+            }
+
+            return response()->json(compact('user'));
+    }
+    
+}
