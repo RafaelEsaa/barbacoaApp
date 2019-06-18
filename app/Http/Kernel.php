87a5a6ebf06f6cfca51b7fc4555c504de5982a90
Rafael Esaa <esaarafael@hotmail.com>
2019-06-18 12:33:35 -0400

    configuration jwt authentication

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,5 +59,7 @@ class Kernel extends HttpKernel
         'guest' => \Barbacoa\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'auth.jwt' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
+        'jwt.refresh' => Tymon\JWTAuth\Middleware\RefreshToken::class,
     ];
 }
--- app/User.php
+++ app/User.php
@@ -2,10 +2,11 @@
 
 namespace Barbacoa;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
@@ -33,5 +34,25 @@ class User extends Authenticatable
 
     public function reservations(){
         return $this->hasMany('App\Reservation');
+    }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+ 
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
